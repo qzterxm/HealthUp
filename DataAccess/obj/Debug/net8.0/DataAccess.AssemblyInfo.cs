613675fd9af15bd0f6b020c828fd5a2fbf08b26c
Natalia Solokha <solokha.nataliy@gmail.com>
2025-11-09 17:21:29 +0200

    .

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68259b9f47955d0dc31f1875e40eae054759502d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed744db09ba2afd695b1b62bce5b70bf828ce7ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
